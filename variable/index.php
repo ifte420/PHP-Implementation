b2895b6ced11c8f2d8f39b5d5abb0c2a2331d41b
ifte420 <ifte430@gmail.com>
2021-01-02 19:53:16 +0600

    variable and variable rules

---
+++ variable/index.php
@@ -0,0 +1,29 @@
+<?php
+    echo "<h1>Class 2 => H.W 2</h1>";
+//   Target echo // "" .. 
+$slash = "<h2>1. //</h2>";
+$double_quotation = "<h2>2. &#34;&#34;</h2>";
+$full_stop = "..";
+
+    echo $slash;
+    echo $double_quotation;
+    echo "<h2>3  $full_stop</h2>";
+
+    echo "<h3>Rules for PHP variables:</h3>";
+    echo "<h4>1. A variable starts with the $ sign, followed by the name of the variable</h4>";
+    echo "<h4>2. A variable name must start with a letter or the underscore character</h4>";
+    echo "<h4>3. A variable name cannot start with a number</h4>";
+    echo "<h4>4. Variable names are case-sensitive (age and AGE are two different variables)</h4>";
+    echo "<h4>5. A variable name in PHP can only contain alpha-numeric characters and    underscores (A-z, 0-9, and _).</h4>";
+    echo "<h4>6. A variable name cannot contain spaces.</h4>";
+?>
+<style>
+    h3{
+        color: red;
+        font-size: 30px;
+    }
+    h4 {
+        font-size: 25px;
+        color: #d35400;
+    }
+</style>
