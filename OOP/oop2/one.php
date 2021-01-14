c39dee500ea02d187d4a0635d93413487b9563a8
ifte420 <ifte430@gmail.com>
2021-01-14 23:41:57 +0600

    public access properties

--- OOP/oop2/one.php
+++ OOP/oop2/one.php
@@ -5,13 +5,20 @@ class car {
     // Properties End
 
     // Method Start
+    function __construct($val="Red"){
+        // echo "HI $val";
+        $this->wheel_color = $val;
+    }
     function wheel(){
         echo "This is car wheel and wheel color $this->wheel_color";
     }
     // Method End
 }
-$ifte = new car;
+$ifte = new car("White");
 $ifte -> wheel();
+echo "</br>";
+$reaid = new car("Red");
+$reaid -> wheel();
 
 
 
