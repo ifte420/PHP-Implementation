51634a8d5a34779a3dcecf4f2b098e083792d5e9
ifte420 <ifte430@gmail.com>
2021-01-15 00:30:55 +0600

    another public property

--- OOP/oop2/one.php
+++ OOP/oop2/one.php
@@ -1,20 +1,22 @@
 <?php
 class car {
     // Properties Start
-    public $wheel_color = "Black";
+    public $wheel_color = "";
+    public $wheel_shape = "";
     // Properties End
 
     // Method Start
-    function __construct($val="Red"){
+    function __construct($color="Black" , $shape="round"){
         // echo "HI $val";
-        $this->wheel_color = $val;
+        $this->wheel_color = $color;
+        $this->wheel_shape = $shape;
     }
     function wheel(){
-        echo "This is car wheel and wheel color $this->wheel_color";
+        echo "This is car wheel, and wheel color $this->wheel_color, and wheel shape is $this->wheel_shape";
     }
     // Method End
 }
-$ifte = new car("White");
+$ifte = new car("White", "triangle");
 $ifte -> wheel();
 echo "</br>";
 $reaid = new car("Red");
